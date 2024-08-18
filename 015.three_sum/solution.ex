c6a22d0f8658febe730a37a7b7c9bf9d72e48f23
Guy Argo <guy.argo@gmail.com>
2024-08-18 14:39:39 -0700

    better elixir solution to 15.three sum using lists

--- 015.three_sum/solution.ex
+++ 015.three_sum/solution.ex
@@ -1,28 +1,24 @@
 defmodule Solution do
   @spec three_sum(nums :: [integer]) :: [[integer]]
   def three_sum(nums) do
-    three_sum_map(Enum.frequencies(nums))
+    xs = nums |> Enum.sort() |> Enum.with_index()
+    three_sum_list(xs, Enumm.reverse(xs))
   end
 
-  def three_sum_map(map) do
-    map
-    |> Enum.flat_map(fn {x, n} -> two_sum(decrement(map, x), x) end)
-    |> Enum.uniq()
-    |> Enum.map(&Tuple.to_list/1)
+  def three_sum_list([], rev_list), do: []
+  def three_sum_list([{x, _}| _], _) when x > 0, do: []
+  def three_sum_list([{x, i}| xis], rev) do
+    List.flatten(two_sum_list(x, i, xis, rev), three_sum_list(xis, rev))
   end
 
-  def decrement(map, x), do: Map.update!(map, x, &(&1 - 1))
-
-  def two_sum(map, sum) do
-    Enum.flat_map(
-        map,
-        fn {x, n} ->
-          if n > 0 and Map.get(decrement(map, x), -(sum+x), 0) > 0 do
-            [sum, x, -(sum+x)] |> Enum.sort |> List.to_tuple() |> List.wrap()
-          else
-            []
-          end
-        end
-    )
+  def two_sum_list(_, _, [], _), do: []
+  def two_sum_list(_, _, _, []), do: []
+  def two_sum_list(x, i, [{y, y_i} | yis], [{z, z_i} | zis]) when y_i >= z_i, do: []
+  def two_sum_list(x, i, [{y, y_i} | y_next] = yis, [{z, z_i} | z_next] = zis) do
+    cond do
+      x + y + z < 0 -> two_sum_list(x, i, y_next, zis)
+      x + y + z > 0 -> two_sum_list(x, i, yis, z_next)
+      true -> [{x, y, z} | two_sum_list(x, i, Enum.drop_while(y_next, &(&1 == y)), z_next)]
+    end
   end
 end
