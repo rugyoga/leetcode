6ad05152ea5e5a977a569e887261a99eccf72584
Guy Argo <guy.argo@gmail.com>
2024-08-18 20:27:38 -0700

    tweaks to 216.combination sum iii

--- 216.combination_sum_III/solution.ex
+++ 216.combination_sum_III/solution.ex
@@ -3,10 +3,10 @@ defmodule Solution do
   def combination_sum3(k, n) do
     combination_sum3_rec(k, n, [], 1, 9)
     |> List.flatten
-    |> Enum.map(&elem(&1, 0))
+    |> Enum.map(fn {list} -> list end)
   end
 
-  def combination_sum3_rec(1, n, used, lo, hi), do: if(n in lo..hi, do: [{[n | used]}], else: [])
+  def combination_sum3_rec(1, n, used, lo, hi), do: if(lo <= n and n <= hi, do: [{[n | used]}], else: [])
   def combination_sum3_rec(k, n, _, lo, _) when k*lo >= n, do: []
   def combination_sum3_rec(k, n, used, lo, hi), do:
     [combination_sum3_rec(k-1, n-lo, [lo | used], lo+1, hi),
